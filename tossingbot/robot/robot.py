555c0e17f21534b2eb23cfab26cab3561a562b99
cc299792458 <cc2018011855@gmail.com>
2024-09-15 09:30:21 -0400

    add grasp

--- tossingbot/robot/robot.py
+++ tossingbot/robot/robot.py
@@ -5,6 +5,7 @@ import pybullet as p
 import pybullet_data
 
 from collections import namedtuple
+from tossingbot.scene.objects import create_box
 
 class BaseRobot:
     """
@@ -97,6 +98,7 @@ class BaseRobot:
         for joint_id in range(num_joints):
             link_name = p.getJointInfo(self.robot_id, joint_id)[12].decode('utf-8')
             self.links[link_name] = joint_id
+        self.tcp_id = self.links.get('tcp_link')
 
     def reset(self):
         """
@@ -131,13 +133,29 @@ class BaseRobot:
                 maxVelocity=self.joints[joint_id].max_velocity
             )
 
+    def set_tcp_pose(self, tcp_pose):
+        """
+        Set the tcp pose
+        """
+        joint_position = self.inverse_kinematics(pose=tcp_pose)
+        self.set_arm_joint_position(position=joint_position)
+
+    def set_tcp_pose_target(self, target_tcp_pose):
+        """
+        Set the target tcp pose
+        """
+        target_joint_position = self.inverse_kinematics(pose=target_tcp_pose)
+        self.set_arm_joint_position_target(target_position=target_joint_position)
+
     def inverse_kinematics(self, pose):
         """
         Calculate inverse kinematics for the given tcp pose.
         """
-        x, y, z, roll, pitch, yaw = pose
+        # x, y, z, roll, pitch, yaw = pose
+        x, y, z, wx, wy, wz, ww = pose
         position = (x, y, z)
-        orientation = p.getQuaternionFromEuler((roll, pitch, yaw))
+        orientation = (wx, wy, wz, ww)
+        # orientation = p.getQuaternionFromEuler((roll, pitch, yaw))
         current_joint_position = self.get_joint_position()
         joint_position = p.calculateInverseKinematics(
             self.robot_id, self.tcp_id, position, orientation,
@@ -159,11 +177,81 @@ class BaseRobot:
         """
         return [p.getJointState(self.robot_id, joint_id)[1] for joint_id in self.controllable_joints]
 
-    def get_tcp_position(self):
+    def get_tcp_pose(self):
         """
-        Get the current position of the end effector.
+        Get the current pose of the TCP.
+        
+        Returns:
+            list: A list containing the TCP position [x, y, z] and orientation as a quaternion [x, y, z, w].
         """
-        return p.getLinkState(self.robot_id, self.tcp_id)[0]
+        # Get the link state which returns position and orientation (as a quaternion)
+        link_state = p.getLinkState(self.robot_id, self.tcp_id)
+        position = link_state[0]  # [x, y, z]
+        orientation = link_state[1]  # Quaternion [x, y, z, w]
+
+        # Combine the position and orientation into a single list
+        tcp_pose = list(position) + list(orientation)
+
+        return tcp_pose
+    
+    def _pose_distance(self, pose1, pose2):
+        """
+        Calculate the distance between two poses.
+        
+        Args:
+            pose1 (list): The first pose as a list [x, y, z, qx, qy, qz, qw].
+            pose2 (list): The second pose as a list [x, y, z, qx, qy, qz, qw].
+            
+        Returns:
+            dict: A dictionary containing 'position_distance' and 'orientation_distance'.
+        """
+        # Calculate the position distance (Euclidean distance)
+        position1 = np.array(pose1[:3])
+        position2 = np.array(pose2[:3])
+        position_distance = np.linalg.norm(position1 - position2)
+
+        # Calculate the orientation distance using quaternion
+        quat1 = np.array(pose1[3:])
+        quat2 = np.array(pose2[3:])
+        
+        # Compute the dot product of two quaternions
+        dot_product = np.dot(quat1, quat2)
+
+        # Clamp the dot product to avoid numerical errors that can cause issues with arccos
+        dot_product = np.clip(dot_product, -1.0, 1.0)
+        
+        # Calculate the angle between the quaternions
+        orientation_distance = 2 * np.arccos(np.abs(dot_product))
+
+        return {
+            'position_distance': position_distance,
+            'orientation_distance': orientation_distance
+        }
+    
+    def is_tcp_reached_target(self, target_pose, position_tolerance=0.01, orientation_tolerance=0.01):
+        """
+        Check if the TCP has reached the target pose.
+        
+        Args:
+            current_pose (list): The current pose of the TCP [x, y, z, qx, qy, qz, qw].
+            target_pose (list): The target pose of the TCP [x, y, z, qx, qy, qz, qw].
+            position_tolerance (float): The tolerance for position difference.
+            orientation_tolerance (float): The tolerance for orientation difference in radians.
+            
+        Returns:
+            bool: True if the TCP is within the specified tolerances, False otherwise.
+        """
+        # Calculate the distance between the current pose and the target pose
+        current_pose = self.get_tcp_pose()
+        distances = self._pose_distance(current_pose, target_pose)
+        
+        # Check if both the position and orientation distances are within the specified tolerances
+        if (distances['position_distance'] <= position_tolerance and
+            distances['orientation_distance'] <= orientation_tolerance):
+            return True
+        else:
+            return False
+
 
 class UR5Robotiq85(BaseRobot):
     def __init__(self, base_position, base_orientation, initial_position=None, visualize_coordinate_frames=False):
@@ -171,11 +259,9 @@ class UR5Robotiq85(BaseRobot):
         self.initial_position = initial_position if initial_position is not None else [
             0.0, -np.pi/2, np.pi/2, -np.pi/2, -np.pi/2, 0, 0.085
         ]
-        self.tcp_id = 8
         self.gripper_range = [0, 0.085]
-
+        
         super().__init__(base_position, base_orientation)
-
         if visualize_coordinate_frames:
             self.visualize_coordinate_frames()
 
@@ -225,6 +311,20 @@ class UR5Robotiq85(BaseRobot):
         """
         super().reset()
         self.reset_gripper()
+
+    def grasp(self, tcp_target_pose):
+        pose_over_target = tcp_target_pose[:2] + [0.365] + tcp_target_pose[3:]
+        while not self.is_tcp_reached_target(target_pose=pose_over_target):
+            self.set_tcp_pose_target(pose_over_target)
+        self.open_gripper()
+        while not self.is_tcp_reached_target(target_pose=tcp_target_pose):
+            self.set_tcp_pose_target(tcp_target_pose)
+        self.close_gripper()
+        while not self.is_tcp_reached_target(target_pose=pose_over_target):
+            self.set_tcp_pose_target(pose_over_target)
+        # # Check if the grasp is success
+        # self.get_gripper_position()
+
 
     def reset_gripper(self):
         """
@@ -338,12 +438,16 @@ if __name__ == '__main__':
     initial_position = [-1.569/2, -1.545, 1.344, -1.371, -1.571, 0.001, 0.085]
     robot = UR5Robotiq85((0, 0.0, 0.0), (0.0, 0.0, 0.0), initial_position=initial_position, visualize_coordinate_frames=True)
     # Add a slider to control the gripper's open length
-    gripper_slider = p.addUserDebugParameter("Gripper Open Length", robot.gripper_range[0], robot.gripper_range[1], robot.initial_position[-1])
+    # gripper_slider = p.addUserDebugParameter("Gripper Open Length", robot.gripper_range[0], robot.gripper_range[1], robot.initial_position[-1])
+
+    # create_box(half_extents=[0.02, 0.02, 0.02], position=[0.5, 0.0, 0.1])
 
     while True:
         # Read the slider value
-        gripper_length = p.readUserDebugParameter(gripper_slider)
+        # gripper_length = p.readUserDebugParameter(gripper_slider)
         # Set the gripper's open length based on the slider value
-        robot.set_gripper_position_target(gripper_length)
+        # robot.set_gripper_position_target(gripper_length)
+        tcp_target_pose = [0.5, 0.0, 0.2, 0.0, 0.0, 0.0, 1.0]
+        robot.grasp(tcp_target_pose=tcp_target_pose)
         p.stepSimulation()
         time.sleep(1./240.)
